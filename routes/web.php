b90c9d542812aa7fa89146e0e664558be7ba0cc5
salsabilamhda <salsabilamahda217@gmail.com>
2025-02-26 12:43:39 +0700

    Implementasi Query Builder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,3 +12,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
