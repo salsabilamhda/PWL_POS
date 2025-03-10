60bb6d7392fb7fd32e3ce104913216cd7acd69fe
salsabilamhda <salsabilamahda217@gmail.com>
2025-03-10 19:52:52 +0700

    praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,26 +20,37 @@ class UserController extends Controller
             'title' => 'Daftar user yang terdaftar dalam sistem'
         ];
 
-        $activeMenu = 'user'; // set menu yang sedang aktif
+        $activeMenu = 'user';
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+
+        return view('user.index', [
+            'breadcrumb' => $breadcrumb, 
+            'page' => $page, 
+            'level' => $level,
+            'activeMenu' => $activeMenu]);
     }
 
     public function list(Request $request)
-     {
-         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
-         return DataTables::of($users)
-             ->addIndexColumn()->addColumn('aksi', function ($user) {
-                 $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-                 $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-                 $btn .= '<form class="d-inline-block" method="POST" action="' .
-                     url('/user/' . $user->user_id) . '">' . csrf_field() . method_field('DELETE') .
-                     '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
-                 return $btn;
-             })->rawColumns(['aksi'])
-             ->make(true);
-     }
+    {
+        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
 
+        //Filter berdasarkan level
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
+
+        return DataTables::of($users)
+            ->addIndexColumn()->addColumn('aksi', function ($user) {
+                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' .
+                    url('/user/' . $user->user_id) . '">' . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })->rawColumns(['aksi'])
+            ->make(true);
+    }
      // Menampilkan halaman form tambah user
     public function create()
     {
